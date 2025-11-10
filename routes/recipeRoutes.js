964d10d36de810025abeda2b63e6fbac6c1d042e
imonsterette <imonsterette@me.com>
2025-11-10 17:31:15 +0300

    feat(api): expose PUT /:id and DELETE /:id routes

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -4,5 +4,7 @@ const c = require('../controllers/recipeController');
 router.get('/', c.list);
 router.get('/:id', c.get);
 router.post('/', c.create);
+router.put('/:id', c.update);
+router.delete('/:id', c.remove);
 
 module.exports = router;
